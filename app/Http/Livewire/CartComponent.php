13a5fd5cbea02c511de9a718f3ede464da5a7774
Maxafangsco <maxmillianafanga@gmail.com>
2021-06-08 15:07:40 +0400

    impliment search query from top search

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,36 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId)
+    {
+        $product =Cart::get($rowId);
+        $qty = $product ->qty + 1;
+        Cart::update($rowId,$qty);
+    }
+
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product ->qty - 1;
+        Cart::update($rowId,$qty);
+    }
+
+    public function destroy($rowId)
+    {
+        // Cart::remove($rowId);
+        Cart::remove($rowId);
+        Session()->flush('success_message','Item has been removed');
+    }
+
+    public function destroyAll()
+    {
+        Cart::destroy();
+        Session()->flush('success_message','Cart has been removed');
+    }
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\CategoryComponent;
+use App\Http\Livewire\DetailsComponent;
+use App\Http\Livewire\SearchComponent;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,8 +29,11 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 
 Route::get('/',HomeComponent::class);
 Route::get('/shop',ShopComponent::class);
-Route::get('/cart',CartComponent::class);
+Route::get('/cart',CartComponent::class)->name('product.cart');
 Route::get('/checkout',CheckoutComponent::class);
+Route::get('/product/{slug}',DetailsComponent::class)->name('product.details');
+Route::get('/product-category/{category_slug}',CategoryComponent::class)->name('product.category');
+Route::get('/search',SearchComponent::class)->name('product.search');
 
 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
